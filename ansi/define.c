095b61a96cdb3789abe86d27ee6be241a0eb3fac
santoshxshrestha <santoshxshrestha@gmail.com>
2025-12-05 15:55:32 +0545

    pre processors

---
+++ ansi/define.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// These are called preprocessor directives. They are instructions to the
+// compiler
+#define MAX_BUFFER_SIZE 1024
+// here we define a constant for maximum buffer size which will be same
+// throughout the program This helps in maintaining consistency and makes it
+// easier to update the value if needed.
+
+int main() {
+  printf("Hello, world\n");
+  printf("Max buffer size is %d\n", MAX_BUFFER_SIZE);
+  return 0;
+}
