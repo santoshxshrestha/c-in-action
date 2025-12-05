c68f8545b162b400cb1d7021d255aef9e04ad554
santoshxshrestha <santoshxshrestha@gmail.com>
2025-12-05 15:41:29 +0545

    flake: update the latest deps

--- flake.nix
+++ flake.nix
@@ -1,26 +1,34 @@
 {
-  description = "C++ DevShell";
+  description = "C/C++ DevShell";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs {
           inherit system;
         };
-      in {
+      in
+      {
         devShells.default = pkgs.mkShell {
           name = "cpp-dev-shell";
 
           buildInputs = [
-            pkgs.clang           # includes clang++
-            pkgs.libcxx          # standard library for clang++
-            pkgs.clang-tools     # clangd, clang-format, etc.
+            pkgs.clang # includes clang++
+            pkgs.libcxx # standard library for clang++
+            pkgs.clang-tools # clangd, clang-format, etc.
             pkgs.cmake
+            pkgs.gcc
           ];
 
           shellHook = ''
@@ -28,6 +36,6 @@
             export CXX=clang++
           '';
         };
-      });
+      }
+    );
 }
-
