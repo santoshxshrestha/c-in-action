d99f26d120ae3a042e939e0663cc1ba163388556
santoshxshrestha <santoshxshrestha@gmail.com>
2025-12-05 19:09:10 +0545

    this is another type of the way to use define

--- ansi/define.c
+++ ansi/define.c
@@ -6,9 +6,11 @@
 // here we define a constant for maximum buffer size which will be same
 // throughout the program This helps in maintaining consistency and makes it
 // easier to update the value if needed.
+#define STRING "HELLO SANTOSH"
 
 int main() {
   printf("Hello, world\n");
   printf("Max buffer size is %d\n", MAX_BUFFER_SIZE);
+  printf("The string is: %s\n", STRING);
   return 0;
 }
