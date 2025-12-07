eae1ec90d79e8d12902a3c4ef9275fc072b88475
santoshxshrestha <santoshxshrestha@gmail.com>
2025-12-08 00:33:47 +0545

    meta exit

--- ansi/meta_for.c
+++ ansi/meta_for.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 int main() {
   for (int i = 0, j = 10; i < 10 && j > 0; i++, j--) {
     printf("i: %d, j: %d\n", i, j);
   }
+
+  int i = 0;
+  for (;;) {
+    printf("i outside loop: %d\n", i);
+    i++;
+    if (i >= 5) {
+      // exit the program
+      exit(0);
+    }
+  }
+
   return 0;
 }
