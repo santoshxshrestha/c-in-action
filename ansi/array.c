705ffa72787e817a683710b8529b8e48dfab4d8a
santoshxshrestha <santoshxshrestha@gmail.com>
2025-12-08 19:19:53 +0545

    Add 2D array function with variable-length array support

--- ansi/array.c
+++ ansi/array.c
@@ -7,11 +7,21 @@ int function(int array[], int lenght_of_array) {
   return sum;
 }
 
+void func(int rows, int cols, int array[][cols]) {
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < cols; j++)
+      printf("%d\n", array[i][j]);
+  }
+}
+
 int main() {
   int array[] = {1, 2, 3, 4, 5};
   int lenght_of_array = sizeof(array) / sizeof(array[0]);
-  printf("The sum of the each element of the array is %d ",
+  printf("The sum of the each element of the array is %d \n\n",
          function(array, lenght_of_array));
+
+  int two_dimenstion[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+  func(3, 3, two_dimenstion);
 
   return 0;
 }
