932cd5cdd1e405fabf6439b8d0a922fe441e5d06
santoshxshrestha <santoshxshrestha@gmail.com>
2025-12-09 00:17:09 +0545

    yikes, this is getting out of head

--- ansi/bit_fields.c
+++ ansi/bit_fields.c
@@ -20,5 +20,35 @@ int main() {
   printf("The value of a: %d\n", c.a);
   printf("The value of b: %d\n", c.b);
 
+  struct Custom scanning;
+  printf("Enter value for a (3 bits): ");
+  // Note: Scanf for bit fields is not directly supported in C.
+  // we cannnot take the address of a bit field, so we need to use a temporary
+  // variable.
+  // also we need to ensure that the input value fits within the bit field
+  // limits. we can use a temporary variable to read the input and then assign
+  // it to the bit field.
+  int temp_a;
+  scanf("%d", &temp_a);
+  if (temp_a < -4 || temp_a > 3) {
+    printf("Value out of range for 3-bit signed field. Please enter a value "
+           "between -4 and 3.\n");
+  } else {
+    scanning.a = temp_a;
+    printf("You entered a: %d\n", scanning.a);
+  }
+  // Alternative direct scanf (not recommended):
+  // scanf("%d", &scanning.a);
+  // Lets continue with b
+  printf("Enter value for b (5 bits): ");
+  int temp_b;
+  scanf("%d", &temp_b);
+  if (temp_b < 0 || temp_b > 31) {
+    printf("Value out of range for 5-bit unsigned field. Please enter a value "
+           "between 0 and 31.\n");
+  } else {
+    scanning.b = temp_b;
+    printf("You entered b: %d\n", scanning.b);
+  }
   return 0;
 }
